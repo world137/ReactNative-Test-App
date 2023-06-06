85942db81aee51537ea5cb10bfbd990b5f721786
world.cyp <world.sk137@gmail.com>
2023-06-06 12:04:15 +0700

    google places

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,29 +1,62 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
-import { DiscoverScreen, HeaderPart, HeaderText, IconPart, SubHeaderText, UserImgStyle } from '../styles/Discover';
+import { DiscoverScreen, HeaderPart, HeaderText, IconPart, Line, SubHeaderText, UserImgStyle } from '../styles/Discover';
 import { UserImg } from '../assets';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 const Discover = () => {
     const navigation = useNavigation();
     useLayoutEffect(() => {
         navigation.setOptions({
-            headerShown: true,
+            headerShown: false,
         });
     }, []);
     return (
         <SafeAreaView style={DiscoverScreen}>
+            {/* <SafeAreaView className="flex-1 bg-white relative"> */}
             <View style={HeaderPart}>
+                {/* <View className="flex-row items-center justify-between px-6"> */}
                 <View>
                     <Text style={HeaderText}>Discover</Text>
                     <Text style={SubHeaderText}>The beauty today</Text>
                 </View>
 
                 <View style={IconPart}>
+                    {/* <View className = "w-7 h-7 bg-gray-400 rounded-md items-center justify-center shadow-lg"> */}
                     <Image source={UserImg} style={UserImgStyle} />
                 </View>
 
-                <View></View>
+            </View>
+            <View className='flex-row items-center bg-white mx-4 rounded-xl py-1 px-4 shadow-lg mt-2'>
+                {/* <View style={Line}> */}
+                {/* <GooglePlacesAutocomplete
+                    placeholder='Search'
+                    onPress={(data, details = null) => {
+                        // 'details' is provided when fetchDetails = true
+                        console.log(data, details);
+                    }}
+                    query={{
+                        key: 'AIzaSyCYt5mZOleR3Uw549i51SB9SyGEgVCyHag',
+                        language: 'en',
+                    }}
+                /> */}
+                
+                    <GooglePlacesAutocomplete
+                        placeholder='Search destination'
+                        minLength={2}
+                        onFail={error => console.error(error)}
+                        onPress={(data, details = null) => {
+                            // 'details' is provided when fetchDetails = true
+                            console.log(data, details);
+                        }}
+                        query={{
+                            key: "AIzaSyCYt5mZOleR3Uw549i51SB9SyGEgVCyHag",
+                            language: 'en',
+                        }}
+
+
+                    />
+               
             </View>
         </SafeAreaView>
 
--- styles/Discover.js
+++ styles/Discover.js
@@ -12,7 +12,7 @@ export const HeaderPart = {
     paddingHorizontal: windowWidth * 0.05,
     marginTop: windowHeight * 0.06,
     alignItems: 'center',
-    justifyContent:'space-between',
+    justifyContent: 'space-between',
 }
 
 export const HeaderText = {
@@ -24,21 +24,42 @@ export const HeaderText = {
 
 export const SubHeaderText = {
     color: '#527283',
-    fontSize: windowWidth * 0.04,
-    fontWeight : 300,
+    fontSize: windowWidth * 0.06,
+    fontWeight: 300,
     marginLeft: windowWidth * 0.02
 }
 
 export const IconPart = {
-    width : windowWidth * 0.06,
-    height : windowWidth * 0.06,
-    backgroundColor : '#A1A1A1', 
-    borderRadius : windowWidth * 0.01,
+    width: windowWidth * 0.1,
+    height: windowWidth * 0.1,
+    backgroundColor: '#A1A1A1',
+    borderRadius: windowWidth * 0.01,
     justifyContent: 'center',
     alignItems: 'center',
+    shadow: '#fff6',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
 }
 
 export const UserImgStyle = {
-    width : "100%",
-    height : "100%",
+    width: "100%",
+    height: "100%",
+}
+
+export const Line = {
+    backgroundColor : "#aaaa",
+    flexDirection : "row",
+    alignItems : "center",
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    paddingHorizontal : windowWidth * 0.1,
+    paddingVertical: windowWidth * 0.01,
+    marginHorizontal: windowWidth * 0.04,
+    marginTop: windowWidth * 0.05,
+    borderRadius: windowWidth * 0.01,
 }
